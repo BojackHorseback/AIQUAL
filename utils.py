7bc96458bb5830549a13bb3bf1a6ddf094cce519
BJ <60893926+BojackHorseback@users.noreply.github.com>
2025-02-26 15:38:44 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,18 +2,61 @@ import streamlit as st
 import hmac
 import time
 import os
-from datetime import datetime
 
 
 # Password screen for dashboard (note: only very basic authentication!)
 # Based on https://docs.streamlit.io/knowledge-base/deploy/authentication-without-sso
+def check_password():
+    """Returns 'True' if the user has entered a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether username and password entered by the user are correct."""
+        if st.session_state.username in st.secrets.passwords and hmac.compare_digest(
+            st.session_state.password,
+            st.secrets.passwords[st.session_state.username],
+        ):
+            st.session_state.password_correct = True
+
+        else:
+            st.session_state.password_correct = False
+
+        del st.session_state.password  # don't store password in session state
+
+    # Return True, username if password was already entered correctly before
+    if st.session_state.get("password_correct", False):
+        return True, st.session_state.username
+
+    # Otherwise show login screen
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("User or password incorrect")
+    return False, st.session_state.username
 
 
-# Get the current date and time in a readable format
-current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+def check_if_interview_completed(directory, username):
+    """Check if interview transcript/time file exists which signals that interview was completed."""
 
-# Set username with the current date and time appended
-st.session_state.username = f"testaccount_{current_time}"
+    # Test account has multiple interview attempts
+    if username != "testaccount":
+
+        # Check if file exists
+        try:
+            with open(os.path.join(directory, f"{username}.txt"), "r") as _:
+                return True
+
+        except FileNotFoundError:
+            return False
+
+    else:
+
+        return False
 
 
 def save_interview_data(
