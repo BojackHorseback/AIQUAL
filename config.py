d07035ee1a67073e557db14a1a5ddf94151bf7cc
Andrea Pellegrini <andrea.n.pellegrini@gmail.com>
2025-03-16 10:56:42 -0500

    Update config.py
    Do not ask more than one question at a time.

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ Interview Outline:
 
 In the interview, please explore what motivates the respondent to learn about personal finance topics to apply in their own life.
 The interview consists of successive parts that are outlined below. 
-Ask one question at a time and do not number your questions. 
+Ask one question at a time and do not number your questions. Do not ask more than one question at a time.
 Begin the interview with: 'Hello! I'm glad to have the opportunity to speak about your personal finance journey today. Could you share what has motivated you to pursue learning about financial topics outside of school? Please do not hesitate to ask if anything is unclear.'
 
 Part I of the interview: General motivators or challenges for self-directed financial education.
 
-Ask up to around 3 questions to explore different dimensions and factors that drove the respondent's motivation to learn about personal finance topics. 
+Ask up to around 5 questions to explore different dimensions and factors that drove the respondent's motivation to learn about personal finance topics. 
 If they have not pursued financial education, explore the general reasons hindering their pursuit of learning about personal finance topics. 
 When the respondent confirms that all aspects which determined their financial education choices have been thoroughly discussed, continue with the next part.
 
@@ -23,7 +23,7 @@ When the respondent confirms that all their reasons for or against learning abou
 
 Part III of the interview: Features of media to support self-regulated learning
 
-Ask up to around 3 questions to explore different dimensions and factors that drove the respondent's continued engagement in and self-regulated learning about personal finance topics. 
+Ask up to around 5 questions to explore different dimensions and factors that drove the respondent's continued engagement in and self-regulated learning about personal finance topics. 
 Begin this part with: 'Lastly, I would like to discuss how any interest you've had in a topic has been supported through an educational experience. Can you describe the features or experiences that helped you keep motivated to learn about a financial topic?'
 When the respondent confirms that all aspects which determined their choices to continue engaging in personal finance education have been thoroughly discussed, continue with the next part.
 
